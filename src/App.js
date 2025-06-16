2e3d7130dad7e2b4636a8f31db601f787117e17a
Vishal Kr Maurya <vishal.mrya14032003@gmail.com>
2025-06-16 15:15:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,62 +6,100 @@ const App = () => {
   const [input,setInput]= useState('');
 
 
-  const calculateResult = (input) => {
+  // const calculateResult = (input) => {
     
-    let result;
-    try{
-      const operators = ['+','-','/','*','%'];
-      let operator = null;
+  //   let result;
+  //   try{
+  //     const operators = ['+','-','/','*','%'];
+  //     let operator = null;
 
-      for(let i=0;i<input.length;i++){
-        if(operators.includes(input[i])){
-          operator=input[i];
-          break;
-        }
-      }
+  //     for(let i=0;i<input.length;i++){
+  //       if(operators.includes(input[i])){
+  //         operator=input[i];
+  //         break;
+  //       }
+  //     }
 
-      if(!operator){
-        if(isNaN(parseFloat(input).toString()))
-          throw new Error('ERR');
-        setInput(parseFloat(input).toString());
-        return;
-      }
+  //     if(!operator){
+  //       if(isNaN(parseFloat(input).toString()))
+  //         throw new Error('ERR');
+  //       // setInput(parseFloat(input).toString());
+  //       // return;
+  //       console.log(input, typeof input);
+  //       return parseFloat(input);
+  //     }
       
-      const [operand1, operand2] = input.split(operator).map(parseFloat);
-      console.log(operand1,operand2);
-      if(isNaN(operand1) || isNaN(operand2)) {
-        console.log('NaN');
-        setInput('Error');
-        return;
+  //     // const [operand1, operand2] = input.split(operator).map(parseFloat);
+  //     // console.log(operand1,operand2);
+  //     // if(isNaN(operand1) || isNaN(operand2)) {
+  //     //   console.log('NaN');
+  //     //   setInput('Error');
+  //     //   return;
+  //     // }
+
+  //     const [operand1, operand2] = input.split(operator);
+  //     console.log(operand1,operand2);
+      
+  //     const op1 = calculateResult(operand1);
+  //     const op2 = calculateResult(operand2);
+
+  //     if(isNaN(op1) || isNaN(op2)){
+  //       throw new Error('error');
+  //     }
+
+
+  //     switch(operator){
+  //       case '+':
+  //         result = op1 + op2;
+  //         break;
+  //       case '-':
+  //         result = op1 - op2;
+  //         break;
+  //       case '*':
+  //         result = op1 * op2;
+  //         break;
+  //       case '/':
+  //         result = op1 / op2;
+  //         break;
+  //       case '%':
+  //         result = op1 % op2;
+  //         break;
+  //       default:
+  //         throw new Error('Invalid Operator');
+  //     }
+
+  //     console.log(result);
+
+  //     return result;
+
+  //   }
+  //   catch(error){
+  //     setInput('Error');
+  //   }
+  // }
+
+  const calculateResult = (input) => {
+    try {
+      // Only allow digits, spaces, and valid operators to prevent code injection
+      if (!/^[\d+\-*/%.()\s]+$/.test(input)) {
+        throw new Error("Invalid characters in input");
       }
 
-      switch(operator){
-        case '+':
-          result = operand1 + operand2;
-          break;
-        case '-':
-          result = operand1 - operand2;
-          break;
-        case '*':
-          result = operand1 * operand2;
-          break;
-        case '/':
-          result = operand1 / operand2;
-          break;
-        case '%':
-          result = operand1 % operand2;
-          break;
-        default:
-          throw new Error('Invalid Operator');
+      // Use Function constructor to evaluate the math expression
+      const result = Function(`"use strict"; return (${input})`)();
+
+      if (typeof result !== "number" || isNaN(result)) {
+        throw new Error("Invalid calculation");
       }
 
-      setInput(result.toString());
-
-    }
-    catch(error){
-      setInput('Error');
+      return result;
+    } 
+    catch (error) {
+      console.error(error.message);
+      return "Error";
     }
   }
+
 
   const handleButton = (value) =>{
     if(value==='Ac'){
@@ -78,7 +116,7 @@ const App = () => {
       //   setInput("Error");
       // }
 
-      calculateResult(input);
+      setInput(calculateResult(input).toString());
     }
     else{
       setInput((prev_value)=> prev_value + value);
